db893e3ebcf6bcaea1e860471f69d4310fccbaec
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-11-03 21:35:56 -0300

    criando os PATH para o menu lateral

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DietComponent } from './core/components/material-body/diet/diet.component';
+import { ExercisesRoutineComponent } from './core/components/material-body/exercises-routine/exercises-routine.component';
+import { LectureComponent } from './core/components/mental-body/lecture/lecture.component';
 import { ProfileComponent } from './core/components/profile/profile.component';
+import { UserListComponent } from './core/components/user-list/user-list.component';
 
 const routes: Routes = [
-  {path: '', component: ProfileComponent}
+  {path: '', component: ProfileComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'exercises', component: ExercisesRoutineComponent},
+  {path: 'diet', component: DietComponent},
+  {path: 'lecture', component: LectureComponent},
+  {path: 'userList', component: UserListComponent}
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LectureComponent } from './components/mental-body/lecture/lecture.component';
+import { DietComponent } from './components/material-body/diet/diet.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, LectureComponent, DietComponent, UserListComponent],
   imports: [
     RouterModule,
     MaterialModule
